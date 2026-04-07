486e483c9cdd80d3c2b4907d8580d50b6bc1f142
luannt <backsimon88@gmail.com>
2026-04-07 14:08:05 +0700

    Add py2app build config for standalone macOS app

---
+++ setup.py
@@ -0,0 +1,57 @@
+"""
+py2app build script for Simple Transfer File Server.
+
+Usage:
+    python setup.py py2app
+
+Output: dist/Simple Transfer File Server.app
+"""
+
+from setuptools import setup
+
+APP     = ["main.py"]
+OPTIONS = {
+    "argv_emulation": False,          # Must be False for Tkinter apps on macOS
+    "iconfile": None,
+    "includes": [
+        "tkinter",
+        "tkinter.ttk",
+        "tkinter.filedialog",
+        "tkinter.scrolledtext",
+        "tkinter.messagebox",
+        "server_http",
+        "server_tftp",
+        "http.server",
+        "socket",
+        "struct",
+        "threading",
+        "queue",
+        "os",
+        "datetime",
+    ],
+    "packages": [],
+    "excludes": [
+        "unittest", "distutils",
+        "pydoc", "doctest",
+        "audioop", "ftplib", "imaplib",
+        "nntplib", "poplib", "smtplib", "telnetlib",
+    ],
+    "plist": {
+        "CFBundleName":             "Simple Transfer File Server",
+        "CFBundleDisplayName":      "Simple Transfer File Server",
+        "CFBundleIdentifier":       "com.backsimon88.simpletransferfile",
+        "CFBundleVersion":          "1.0.0",
+        "CFBundleShortVersionString": "1.0.0",
+        "NSHighResolutionCapable":  True,
+        "LSMinimumSystemVersion":   "10.13.0",
+        "NSHumanReadableCopyright": "© 2026 backsimon88",
+    },
+}
+
+setup(
+    name="Simple Transfer File Server",
+    app=APP,
+    data_files=[],
+    options={"py2app": OPTIONS},
+    setup_requires=["py2app"],
+)
